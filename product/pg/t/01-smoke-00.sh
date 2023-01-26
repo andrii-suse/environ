40dc470d315faddb0044bbc967fce26ad30908bb
Andrii Nikitin <anikitin@suse.de>
2023-01-26 16:13:32 +0100

    Add sql_loop and sql_test scripts

--- product/pg/t/01-smoke-00.sh
+++ product/pg/t/01-smoke-00.sh
@@ -5,11 +5,18 @@ pg=$(environ pg)
 $pg/start
 $pg/status
 
-$pg/create_db test
-$pg/sql_test "create table t1 as select 'x1'"
+$pg/create_db tst
+$pg/sql_tst "create table t1 as select 'x1'"
 
-test x1 == $($pg/sql -t -c "select * from t1" test)
-test x1 == $($pg/sql_test "select * from t1")
+test x1 == $($pg/sql -t -c "select * from t1" tst)
+test x1 == $($pg/sql_tst "select * from t1")
+
+$pg/sql_test x1 == "select * from t1"
+$pg/sql_test x2 != "select * from t1"
+
+rc=0
+$pg/sql_test x2 == "select * from t1" || rc=$?
+test $rc -gt 0
 
 $pg/stop
 
